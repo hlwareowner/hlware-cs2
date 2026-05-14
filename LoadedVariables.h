6ca99ab36db2c925e03f643027851571abdde87d
hlwareowner <hlware@proton.me>
2026-05-14 12:27:40 -0300

    Update LoadedVariables.h

--- LoadedVariables.h
+++ LoadedVariables.h
@@ -5,6 +5,8 @@
 
 #include <cstdint>
 
+typedef double(__fastcall* CreateMoveFn)(CCSGOInput* pThis, unsigned int slot, CUserCommandStruct* pCmd);
+
 namespace LoadedVariables {
 	inline uintptr_t ClientDLL;
 	inline uintptr_t ViewMatrix;
@@ -12,10 +14,14 @@ namespace LoadedVariables {
 	inline uintptr_t LocalPlayerPawn;
 	inline uintptr_t EntityList;
 	inline uintptr_t attack;
-
+	
 	inline InitTraceData_t fnInitTraceData;
 	inline InitTraceInfo_t fnInitTraceInfo;
 	inline InitTraceFilter_t fnInitFilter;
 	inline CreateTrace_t fnCreateTrace;
 	inline GetTraceInfo_t fnGetTraceInfo;
+	inline HandleBulletPen_t fnHandleBulletPenetration;
+	inline CreateMoveFn CreateMove;
+
+	inline CUserCommandStruct* UserCommand;
 }
